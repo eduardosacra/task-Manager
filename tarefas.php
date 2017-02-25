e5c9f520c34a01ae154925ff3e491ac6f4595941
Eduardo <eduardo.sacra.sousa@gmail.com>
2017-02-25 11:53:39 -0300

    first commit

---
+++ tarefas.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+  <title>Gerenciador de Tarefas</title>
+  <link rel="stylesheet" href="/css/styletarefas.css">
+</head>
+  <body>
+    <h1>Gerenciador de Tarefas</h1>
+    <form>
+      <fieldset>
+        <legend>Nova Tarefa</legend>
+        <label>
+          Tarefa:
+          <input type="text" name="nome" />
+        </label>
+        <input type="submit" value="Cadastrar" />
+      </fieldset>
+    </form>
+  </body>
+</html>
