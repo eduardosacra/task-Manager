df9e353782b892b57db1fd7df93dfd191c76bf3b
Eduardo <eduardo.sacra.sousa@gmail.com>
2017-02-28 21:33:25 -0300

    added tasks session  style page

--- tarefas.php
+++ tarefas.php
@@ -1,7 +1,8 @@
+<?php session_start();   ?>
 <html>
 <head>
   <title>Gerenciador de Tarefas</title>
-  <link rel="stylesheet" href="/css/styletarefas.css">
+  <link rel="stylesheet" href="/css/tarefas.css">
 </head>
   <body>
     <h1>Gerenciador de Tarefas</h1>
@@ -15,5 +16,29 @@
         <input type="submit" value="Cadastrar" />
       </fieldset>
     </form>
+
+    <?php
+      if(isset($_GET['nome'])) {
+        $_SESSION['lista_tarefas'][]= $_GET['nome'];
+
+      }
+
+      $lista_tarefas = array();
+
+      if(isset($_SESSION['lista_tarefas'])){
+        $lista_tarefas = $_SESSION['lista_tarefas'];
+      }
+     ?>
+
+     <table>
+      <tr>
+        <th>Tarefas</th>
+      </tr>
+      <?php foreach ($lista_tarefas as $key => $value) :?>
+      <tr>
+        <td><?php echo $value; ?></td>
+      </tr>
+    <?php endforeach ?>
+     </tables>
   </body>
 </html>
